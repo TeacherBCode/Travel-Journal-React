1390e1c810f0cc6c512552b8db3b7799032f5835
Teacher B <156999512+TeacherBCode@users.noreply.github.com>
2024-04-25 13:00:27 +0530

    Initial Files

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Card from './components/Card.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Card />
+  </>
+)
